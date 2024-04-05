b72dd77919657c9a034c6484e83ace5a7de4e2c9
Mavgarv01 <152499842+Mavgarv01@users.noreply.github.com>
2024-04-05 11:25:31 +0530

    Add files via upload

--- create_dataset.py
+++ create_dataset.py
@@ -1,64 +1,55 @@
+# I am using mediapipe as a hand detector and landmark detector and a Random Forest classifier as sign classifier.
+
+
+
 import os
-import cv2
 import pickle
 import mediapipe as mp
-
+import cv2
+import matplotlib.pyplot as plt
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(
-    static_image_mode=True, 
-    min_detection_confidence=0.5,
-    max_num_hands=1
-)
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
+DATA_DIR = 'C:/Users/Dell/Desktop/aslgooglenew/data'
 
-def generate_keypoints(hands, image_path):
-    data_aux = []
-    img = cv2.imread(image_path)
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(img_rgb)
+data = []
+labels = []
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
 
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            x_values = [lm.x for lm in hand_landmarks.landmark]
-            y_values = [lm.y for lm in hand_landmarks.landmark]
+        x_ = []
+        y_ = []
 
-            for i in range(len(hand_landmarks.landmark)):
-                data_aux.append(x_values[i] - min(x_values))
-                data_aux.append(y_values[i] - min(y_values))
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    return data_aux
-    
+        results = hands.process(img_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
 
-def build_dataset(hands, data_dir, letter=True):
-    data = []
-    labels = []
+                    x_.append(x)
+                    y_.append(y)
 
-    for label_dir in os.listdir(data_dir):
-        if letter and (not label_dir.isalpha() and label_dir != "UNKNOWN_LETTER"):
-            continue
-        if not letter and (not label_dir.isnumeric() and label_dir != "UNKNOWN_NUMBER"):
-            continue
-        for image_path in os.listdir(os.path.join(data_dir, label_dir)):
-            image_path = os.path.join(data_dir, label_dir, image_path)
-            data_aux = generate_keypoints(hands, image_path)
+                print("Length of x_:", len(x_))
+                print("Length of y_:", len(y_))
 
-            # Save "Ascii" label or "Unknown" label
-            if data_aux:
-                data.append(data_aux)
-                labels.append(label_dir)
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
 
-    # Save Dataset to pickle file
-    saved_name = "data" if letter else "number"
-    with open(f'{data_dir}/{saved_name}.pickle', 'wb') as dataset:
-        pickle.dump({'data': data, 'labels': labels}, dataset)
+            data.append(data_aux)
+            labels.append(dir_)
 
-    # Display all unique elements in labels sorted by length and alphabetically
-    print(f"Labels: {sorted(set(labels), key=lambda item: (len(item), item))}")
-    
-
-if __name__ == '__main__':
-    build_dataset(hands, data_dir='.')
+f = open('data1.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
